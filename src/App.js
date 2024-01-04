2f658847393257d0ae1d6a0f9d4db577e7e426df
Andrychess <144808434+Andrychess@users.noreply.github.com>
2024-01-04 13:47:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const greeting = "Это тестовый проект Кушнеров А.С.";
+  const greeting = "Это тестовый проект Пономарев А.Е.";
 
   return (
     <div className="App">
